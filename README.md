db0d745330aca2012c14278f2271d8c8dc73a24d
Sarvar019 <megafon.kcr.09.09@gmail.com>
2025-02-09 16:02:57 +0500

    Day 2: Intro Autolayout, completed

--- README.md
+++ README.md
@@ -16,3 +16,11 @@ Swift Arcade
 the first video in playlist is about first intro how to professionally building ios app flow. Also we learnt creating project without storyboard, explaining vector assets and its usefullness. They are mathematically calculated and that's why very looks beautifull, discussing auto-layout 
 
 
+## Day 2 
+- Intro auto layout 
+- what auotlyout is ?
+autolayout is mathematically constraints that apple uses dynamically layout and postions its views
+this is how our app able to render themselves with one set of definiton  on variety of differently sized devices
+
+today we're introduced how ios app can render themselves by using autolayout. And how to use them rightly and pottentilally.
+
--- Weathery/Weathery/WeatherViewController.swift
+++ Weathery/Weathery/WeatherViewController.swift
@@ -9,7 +9,11 @@ import UIKit
 
 class WeatherViewController: UIViewController {
 
-    var backgroundView = UIImageView()
+    let backgroundView = UIImageView()
+    let locationButton = UIButton()
+    let searchButton = UIButton()
+    let searchTextField = UITextField()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         style()
@@ -23,17 +27,54 @@ extension WeatherViewController {
         backgroundView.translatesAutoresizingMaskIntoConstraints = false
         backgroundView.image = UIImage(named: "day-background")
         backgroundView.contentMode = .scaleAspectFill
+        
+        locationButton.translatesAutoresizingMaskIntoConstraints = false
+        locationButton.setBackgroundImage(UIImage(systemName: "location.north.circle.fill"), for: .normal)
+        locationButton.tintColor = .label
+        
+        searchButton.translatesAutoresizingMaskIntoConstraints = false
+        searchButton.setBackgroundImage(UIImage(systemName: "magnifyingglass"), for: .normal)
+        searchButton.tintColor = .label
+        
+        searchTextField.translatesAutoresizingMaskIntoConstraints = false
+        searchTextField.placeholder = "Search"
+        searchTextField.borderStyle = .roundedRect
+        searchTextField.backgroundColor = .systemFill
+        searchTextField.becomeFirstResponder()
+        searchTextField.textAlignment = .left
+        searchTextField.font = UIFont.preferredFont(forTextStyle: .title1)
     }
     
     func layout() {
         view.addSubview(backgroundView)
+        view.addSubview(locationButton)
+        view.addSubview(searchButton)
+        view.addSubview(searchTextField)
+        
         NSLayoutConstraint.activate([
+            // backgroundView
             backgroundView.topAnchor.constraint(equalTo: view.topAnchor),
             backgroundView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
             backgroundView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
-            backgroundView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+            backgroundView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            
+            // locationButton
+            locationButton.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            locationButton.leadingAnchor.constraint(equalToSystemSpacingAfter: view.leadingAnchor, multiplier: 1),
+            locationButton.widthAnchor.constraint(equalToConstant: 40),
+            locationButton.heightAnchor.constraint(equalToConstant: 40),
+            
+            // searchButton
+            searchButton.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            view.trailingAnchor.constraint(equalToSystemSpacingAfter: searchButton.trailingAnchor, multiplier: 1),
+            searchButton.widthAnchor.constraint(equalToConstant: 40),
+            searchButton.heightAnchor.constraint(equalToConstant: 40),
+            
+            // searchTextField
+            searchTextField.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            searchTextField.leadingAnchor.constraint(equalToSystemSpacingAfter: locationButton.trailingAnchor, multiplier: 1),
+            searchButton.leadingAnchor.constraint(equalToSystemSpacingAfter: searchTextField.trailingAnchor, multiplier: 1),
         ])
     }
-    
 }
 
